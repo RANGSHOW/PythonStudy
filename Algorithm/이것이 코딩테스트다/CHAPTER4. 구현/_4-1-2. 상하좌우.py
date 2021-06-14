2ca84b09114f0ca82010e1261be5243684d35a15
RANGSHOW <rangshow@gmail.com>
2021-06-15 02:10:29 +0900

    DFS 예제

--- Algorithm/이것이 코딩테스트다/CHAPTER4. 구현/_4-1-2. 상하좌우.py
+++ Algorithm/이것이 코딩테스트다/CHAPTER4. 구현/_4-1-2. 상하좌우.py
@@ -16,3 +16,4 @@ def move(xloc, yloc, singleMove):
 if __name__ == "__main__":
     pass
 
+
--- Algorithm/프로그래머스/코딩테스트 연습/DFS, BFS/타겟 넘버.py
+++
@@ -1,4 +0,0 @@
-numbers = [1, 1, 1, 1, 1]  # n 개의 움이 아닌 정수 
-target = 3  # 
-returnNum = 5  # 더하거나 빼서 target 을 만들 수 있는 방법 수 
-
